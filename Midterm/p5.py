b0403174f9b508b9ed3ae956faeee78b46a811fb
mayukh2002 <dasmayukh2002@gmail.com>
2022-12-14 13:36:24 -0800

    long time no commit

--- Midterm/p5.py
+++ Midterm/p5.py
@@ -1,5 +1,21 @@
 
 
+# def print_without_vowels(s):
+#     '''
+#     s: the string to convert
+#     Finds a version of s without vowels and whose characters appear in the
+#     same order they appear in s. Prints this version of s.
+#     Does not return anything
+#     '''
+#
+    vowel_set = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+    ans = ""
+
+    for c in s:
+        if c not in vowel_set:
+            ans += c
+    print(ans)
+
 def print_without_vowels(s):
     '''
     s: the string to convert
@@ -8,13 +24,13 @@ def print_without_vowels(s):
     Does not return anything
     '''
 
-    vowel_set = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-    ans = ""
-
-    for c in s:
-        if c not in vowel_set:
-            ans += c
-    print(ans)
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    letter = []
+    for i in s:
+        if i not in vowels:
+            #letter.append(i)
+            print(i)
+    # print(str(letter))
 
 
 if __name__ == '__main__':
